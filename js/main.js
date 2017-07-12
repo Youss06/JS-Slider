1775e5f7b25ebd6944dc2722de4fa6a582ba3213
Youss06 <oumar.youssouf0605@gmail.com>
2017-07-12 19:25:35 +0200

    Add change

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
-var caroussel=document.getElementsByClassName('slider');
-var index;
+var images = document.getElementsByTagName('img');
+var index = 0 ;
 
-function afficherSuivante(){
+setInterval(2000);
 
+function suivant(){
+  console.log(index);
+  images[index].style.display="none";
+  if (index==2) {
+    index=0;
+  } else {
+    index++;
+images[index].style.display="block";
+  }
 }
 
-setTimeout(afficherSuivante(),2000);
+
+function precedent() {
+   images[index].style.display="none";
+   if (index==0) {
+     index=2;
+   } else {
+    index--;
+    images[index].style.display="block";
+  }
+}
